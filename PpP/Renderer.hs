a1d568f44735cbe83310265cbe70b7d1a41657c3
Thhethssmuz <thhethssmuz@gmail.com>
2014-11-11 06:34:11 +0100

    added a simple cv renderer

--- PpP/Renderer.hs
+++ PpP/Renderer.hs
@@ -1,5 +1,7 @@
 module PpP.Renderer (
+  module PpP.Renderer.CV,
   module PpP.Renderer.Report
   ) where
 
+import PpP.Renderer.CV
 import PpP.Renderer.Report
---
+++ PpP/Renderer/CV.hs
@@ -0,0 +1,83 @@
+module PpP.Renderer.CV (renderCV) where
+
+import PpP.Err
+import PpP.Filter
+import PpP.Shared
+import PpP.Language
+import PpP.Renderer.Shared
+
+import Text.Pandoc
+import Text.Pandoc.PDF
+import Text.Pandoc.Walk
+import qualified Data.ByteString.Lazy.Char8 as BS
+
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State
+import Data.Ord (comparing)
+import Data.Char (toLower)
+import Data.List (transpose, sortBy)
+import Data.Maybe (fromMaybe, isJust)
+
+import Paths_ppp
+import System.FilePath
+import System.Exit
+
+
+configure :: Unprocessed -> State PpP ()
+configure (Markdown s) = add "" s
+configure (Macro k v)  = case k of
+
+  "author"          -> addOnce k $ metaVar k v
+  "address"         -> addOnce k . metaVar k . concat 
+                     $ ["\naddress" ++ show i ++ ": " ++ x | (i,x) <- zip [1..] (parseList v)]
+  "phone"           -> addOnce k $ metaVar k v
+  "email"           -> addOnce k $ metaVar k v
+  "webpage"         -> addOnce k $ metaVar k v
+
+  "page-size"       -> addOnce k $ metaVar k v
+  "page-div"        -> addOnce k $ metaVar k v
+  "page-numbers"    -> addOnce k $ metaVar k "true"
+
+  "font-size"       -> addOnce k $ metaVar k v
+  "main-font"       -> addOnce k $ metaVar k v
+  "sans-font"       -> addOnce k $ metaVar k v
+  "mono-font"       -> addOnce k $ metaVar k v
+
+  _                 -> add "err" . pppErr $ "unknown macro " ++ k
+
+tableHack :: Block -> Block
+tableHack (Table is as (w:x:[]) hs tcs) = Table is as [0.66, 0.33 + 0.05] hs tcs
+tableHack x = x
+
+renderCV :: [Unprocessed] -> FilePath -> IO ()
+renderCV doc out = do
+  template  <- readFile =<< getDataFileName ("tex" </> "cv.tex")
+
+  let ppp    = execState (mapM_ configure doc) emptyPpP{
+                 reader = def{
+                   readerSmart = True,
+                   readerStandalone = True,
+                   readerParseRaw = True
+                 },
+                 writer = def{
+                   writerStandalone = True,
+                   writerChapters = False,
+                   writerTemplate = template
+                 }
+               }
+
+  let pandoc = walk tableHack
+             . readMarkdown (reader ppp)
+             $ document ppp
+
+  printErrors pandoc
+
+  putStrLn $ "rendering " ++ out
+
+  pdf <- makePDF "xelatex" writeLaTeX (writer ppp) pandoc
+
+  case pdf of
+    Left err -> do
+                BS.putStrLn err
+                exitFailure
+    Right bs -> BS.writeFile out bs
