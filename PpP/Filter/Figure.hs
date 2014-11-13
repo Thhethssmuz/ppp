50c5a2caa2c85d73bfc9d8c8511e7675dac48957
Thhethssmuz <thhethssmuz@gmail.com>
2014-11-13 01:54:42 +0100

    added one-column figure variant for two-column mode

--- PpP/Filter/Figure.hs
+++ PpP/Filter/Figure.hs
@@ -9,11 +9,11 @@ import Text.Pandoc.Walk
 import Data.List.Split (splitOn)
 import Numeric         (showFFloat)
 import Data.List       (genericLength)
-import Data.Maybe      (maybeToList)
+import Data.Maybe      (maybeToList, isJust)
 import Data.Char       (toLower)
 
 type Scale   = Float
-data Pos     = L | C | R deriving (Show)
+data Pos     = L | C | F | R deriving (Show, Eq)
 type Loc     = (Scale, Pos)
 type Ref     = String
 type Caption = [Inline]
@@ -28,27 +28,27 @@ parseScale = do
   char '%'
   return . read $ p ++ n
 
-parseLoc :: Parser Loc
-parseLoc = do
+parseLoc :: Bool -> Parser Loc
+parseLoc b = do
   l <- option ' ' $ char '<'
   p <- parseScale
   r <- option ' ' $ char '>'
 
   case [l, r] of "< " -> return (p, L)
-                 "<>" -> return (p, C)
+                 "<>" -> return (p, if b then F else C)
                  "  " -> return (p, C)
                  " >" -> return (p, R)
 
-processTarget :: Target -> (Scale, Pos, [FilePath], Ref)
-processTarget (fp, r) =
+processTarget :: Bool -> Target -> (Scale, Pos, [FilePath], Ref)
+processTarget b (fp, r) =
   let  fps = splitOn "%20" fp in
-  case parse parseLoc "" (last fps) of 
+  case parse (parseLoc b) "" (last fps) of
     Left    _   -> (1.0, C, fps, r)
     Right (s,p) -> (s / 100.0, p, init fps, r)
 
 
 processCaptions :: [Inline] -> [Caption]
-processCaptions xs = foldr f [[]] xs where 
+processCaptions xs = foldr f [[]] xs where
   f (Str x) []     = [[Str x]]
   f (Str x) (a:as) = if elem ';' x
                      then let s = splitOn ";" x in
@@ -57,9 +57,9 @@ processCaptions xs = foldr f [[]] xs where
   f x       (a:as) = (x:a):as
 
 
-fromImage :: [Inline] -> Target -> Figure
-fromImage is t = Figure cs fs p s r
-  where (s, p, fs, r) = processTarget t
+fromImage :: Bool -> [Inline] -> Target -> Figure
+fromImage b is t = Figure cs fs p s r
+  where (s, p, fs, r) = processTarget b t
         cs            = processCaptions is
 
 
@@ -71,16 +71,32 @@ showF f = showFFloat (Just 4) f ""
 toInline :: Figure -> [Inline]
 toInline (Figure _  []     _ _ _) = error "missing image url"
 
-toInline (Figure cs (f:[]) C s r) = 
+toInline (Figure cs (f:[]) F s r) =
   let a = do
           c  <- take 1 cs
           c' <- take 1 c
           [ tex $ "\\caption{" ] ++ c ++ [
             tex $ "}\n",
             tex $ "\\label{" ++ r ++ "}\n"
-            ] 
+            ]
   in [
-    tex $ "\\begin{figure}[H]",
+    tex $ "\\begin{figure*}[tbp]\n",
+    tex $ "\\centering\n",
+    tex $ "\\includegraphics[width=" ++ showF s ++ "\\textwidth]{" ++ f ++ "}\n" ]
+    ++ a ++ [
+    tex $ "\\end{figure*}"
+    ]
+
+toInline (Figure cs (f:[]) C s r) =
+  let a = do
+          c  <- take 1 cs
+          c' <- take 1 c
+          [ tex $ "\\caption{" ] ++ c ++ [
+            tex $ "}\n",
+            tex $ "\\label{" ++ r ++ "}\n"
+            ]
+  in [
+    tex $ "\\begin{figure}[H]\n",
     tex $ "\\centering\n",
     tex $ "\\includegraphics[width=" ++ showF s ++ "\\textwidth]{" ++ f ++ "}\n" ]
     ++ a ++ [
@@ -88,29 +104,26 @@ toInline (Figure cs (f:[]) C s r) =
     tex $ "\\end{figure}"
     ]
 
-toInline (Figure cs (f:[]) p s r) = 
+toInline (Figure cs (f:[]) p s r) =
   let s' = showF s ++ "\\textwidth"
       a  = do
            c  <- take 1 cs
            c' <- take 1 c
-           [ tex $ "\\vspace{-24pt}\n",
-             tex $ "\\caption{" ] ++ c ++ [
+           [ tex $ "\\caption{" ] ++ c ++ [
              tex $ "}\n",
              tex $ "\\label{" ++ r ++ "}\n"
-             ] 
+             ]
   in [
     tex $ "\\begin{wrapfigure}{" ++ (map toLower (show p)) ++ "}{" ++ s' ++ "}\n",
-    tex $ "\\vspace{-13pt}\n",
     tex $ "\\centering\n",
     tex $ "\\includegraphics[width=" ++ s' ++ "]{" ++ f ++ "}\n" ]
     ++ a ++ [
-    tex $ "\\vspace{-11pt}\n",
     tex $ "\\end{wrapfigure}"
     ]
 
-toInline (Figure cs fs     _ s r) =
+toInline (Figure cs fs     p s r) =
   let s' = showF $ s / genericLength fs - 0.01 in [
-  tex $ "\\begin{figure}[H]\n",
+  tex $ "\\begin{figure" ++ (if p == F then "*}[tbp]\n" else "}[H]\n"),
   tex $ "\\centering\n"] ++ do
 
   (f, i) <- zip fs [1..]
@@ -136,17 +149,18 @@ toInline (Figure cs fs     _ s r) =
        tex $ "\\label{" ++ r ++ "}\n"
        ]
 
-  ++ [ tex $ "\\vspace{-10pt}\n",
-       tex $ "\\end{figure}" ]
+  ++ [ tex $ "\\end{figure" ++ (if p == F then "*" else "") ++  "}" ]
 
 wrap :: [Inline] -> Inline
 wrap is = Span ("", [], []) $ [pre] ++ is ++ [post]
   where pre  = tex "}"
         post = tex "{"
 
-processFigures :: Inline -> Inline
-processFigures (Image is to) = wrap . toInline $ fromImage is to
-processFigures x = x
+processFigures :: Bool -> Inline -> Inline
+processFigures b (Image is to) = wrap . toInline $ fromImage b is to
+processFigures _ x = x
 
 figure :: Pandoc -> Pandoc
-figure = walk processFigures
+figure pandoc@(Pandoc meta _) =
+  let twocolumn = isJust . lookupMeta "page-twocolumn" $ meta
+  in  walk (processFigures twocolumn) pandoc
