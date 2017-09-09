d978580c85fa6495b03729b368d34951a6f80267
Thhethssmuz <thhethssmuz@gmail.com>
2017-09-09 12:09:01 +0200

    add multicol filter

---
+++ src/Filter/Multicol.hs
@@ -0,0 +1,20 @@
+module Filter.Multicol (multicol) where
+
+import Text.Pandoc.Definition
+import Text.Pandoc.Walk
+
+tex :: String -> Block
+tex = RawBlock (Format "tex")
+
+wrap :: Block -> Block
+wrap b = Div ("",[],[]) [ tex $ "\\end{pppmulticol}", b, tex $ "\\begin{pppmulticol}" ]
+
+columnize :: Bool -> Block -> Block
+columnize False b@(Header 1 _ _) = wrap b
+columnize _     b = b
+
+multicol :: Pandoc -> Pandoc
+multicol doc@(Pandoc meta _) = walk (columnize b) doc
+  where b = case lookupMeta "documentclass" meta of
+              Just (MetaString "scrartcl") -> True
+              _                            -> False
