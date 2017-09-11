e09f626348b4dd527a96a9d522ec667fc4eef126
Thhethssmuz <thhethssmuz@gmail.com>
2017-09-11 18:47:53 +0200

    conditionally run links as notes filter

--- src/Filter/LinksAsNotes.hs
+++ src/Filter/LinksAsNotes.hs
@@ -1,5 +1,6 @@
 module Filter.LinksAsNotes (linksAsNotes) where
 
+import Data.Maybe (isJust)
 import Text.Pandoc.Definition
 import Text.Pandoc.Walk
 
@@ -9,5 +10,10 @@ f (Link attr is (url, title)) =
   Span ("",[],[]) $ is ++ [Space, Note [Para [Link attr [Str url] (url, title)]]]
 f x = x
 
+linksAsNotes' :: Pandoc -> Pandoc
+linksAsNotes' = walk f
+
 linksAsNotes :: Pandoc -> Pandoc
-linksAsNotes = walk f
+linksAsNotes doc@(Pandoc meta _)
+  | isJust $ lookupMeta "links-as-notes" meta = linksAsNotes' doc
+  | otherwise = doc
