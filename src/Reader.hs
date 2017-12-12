8ea243bb8ed2ef1dde7439c40a6fc2921a3c9fee
Thhethssmuz <thhethssmuz@gmail.com>
2017-12-12 09:53:09 +0100

    update reader to pandoc v2

--- src/Reader.hs
+++ src/Reader.hs
@@ -1,13 +1,16 @@
 module Reader (reader, toPandoc) where
 
+import qualified Data.Text as T
+import Text.Pandoc.Class (runIOorExplode)
+import Text.Pandoc.Definition (Pandoc)
+import Text.Pandoc.Options
 import Text.Pandoc.Readers.Markdown (readMarkdown)
-import Text.Pandoc.Options (ReaderOptions(..), def)
 
 reader :: ReaderOptions
 reader = def
-  { readerSmart      = True
-  , readerStandalone = True
-  , readerParseRaw   = True
+  { readerStandalone = True
+  , readerExtensions = disableExtension Ext_table_captions pandocExtensions
   }
 
-toPandoc = readMarkdown reader
+toPandoc :: String -> IO Pandoc
+toPandoc = runIOorExplode . readMarkdown reader . T.pack
